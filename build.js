ea2ed3a44a33d9d95ffdb83db4091c2aed07ad74
takumus <me@takum.us>
2020-03-30 20:00:19 +0900

    ダイエット＆おしゃれ

--- build.js
+++ build.js
@@ -2,26 +2,24 @@ const cp = require('child_process');
 const buildMode = require('./package.json').buildSettings.buildMode;
 const npx = [/^win/.test(process.platform) ? 'npx.cmd' : 'npx'];
 const tasks = [
-  { name: 'rimraf', args: ['rimraf', './dist'] },
-  { name: 'cjs', args: ['rollup', '-c', 'rollup-cjs.config.js'] },
-  { name: 'esm', args: ['rollup', '-c', 'rollup-esm.config.js'] },
-  { name: 'browser', args: ['rollup', '-c', 'rollup-browser.config.js'] },
-  { name: 'd.ts', args: ['tsc', '--emitDeclarationOnly'] }
+  ['rimraf', './dist'],
+  ['rollup', '-c', 'rollup-cjs.config.js'],
+  ['rollup', '-c', 'rollup-esm.config.js'],
+  ['rollup', '-c', 'rollup-browser.config.js'],
+  ['tsc', '--emitDeclarationOnly']
 ];
-const taskLength = tasks.length;
-let taskCount = 0;
-function buildParallel() { tasks.forEach((task) => build(task)); };
-function buildSerial(task) { if (task) build(task, () => buildSerial(tasks.shift())); };
+function buildParallel(tasks) { tasks.forEach((task) => build(task)); };
+function buildSerial(tasks, i) { if (tasks[i]) build(tasks[i], () => buildSerial(tasks, i + 1)); };
 function build(task, callback) {
-  cp.exec([...npx, ...task.args].join(' '), (error, stdout, stderr) => {
-    process.stdout.write(`\n(${++taskCount}/${taskLength})[${task.name}]\n` + (error || stdout || stderr));
-    if(callback) callback();
+  cp.exec([...npx, ...task].join(' '), (error, stdout, stderr) => {
+    process.stdout.write(`\n> ${task.join(' ')}\n` + (error || stdout || stderr));
+    if (callback) callback();
   });
 }
 if (buildMode == 'serial') {
   console.log('begin serial build');
-  buildSerial(tasks.shift());
-}else {
+  buildSerial(tasks, 0);
+} else {
   console.log('begin paralell build');
-  buildParallel();
+  buildParallel(tasks);
 }
